ea413fede513f4de61f4588e4ecb9c2d6213e21f
Rafael Longo <rafaelongo45@gmail.com>
2024-10-19 15:50:06 -0300

    feat: includes volume in docker compose archive and creates database file for sqlite

--- start-backend.sh
+++ start-backend.sh
@@ -1,4 +1,4 @@
 export FLASK_APP="run.py"
 export FLASK_DB_TYPE="sqlite"            # Use SQLITE
-export FLASK_DB_PATH="/api/database.db" # caminho do banco
+export FLASK_DB_PATH="/api//database/database.db" # caminho do banco
 flask --app /api/run.py run --host=0.0.0.0 --port=5000
