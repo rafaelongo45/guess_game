49c6ec32a2bd6d2a38c3422ee20cbab00a80eb59
Rafael Longo <rafaelongo45@gmail.com>
2024-11-10 16:14:04 -0300

    feat: alters database from sqlite to postgres

--- start-backend.sh
+++ start-backend.sh
@@ -1,4 +1,8 @@
 export FLASK_APP="run.py"
-export FLASK_DB_TYPE="sqlite"            # Use SQLITE
-export FLASK_DB_PATH="/api//database/database.db" # caminho do banco
+export FLASK_DB_TYPE="postgres"
+export FLASK_DB_USER="postgres"
+export FLASK_DB_NAME="postgres"
+export FLASK_DB_PASSWORD="secretpass"
+export FLASK_DB_HOST="db"
+export FLASK_DB_PORT="5432"
 flask --app /api/run.py run --host=0.0.0.0 --port=5000
